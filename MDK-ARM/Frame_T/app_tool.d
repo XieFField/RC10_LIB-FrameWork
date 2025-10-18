ef7a7ba3e192441fe01892fcf4179b38dd464ed2
70er66 <2602347865@qq.com>
2025-10-18 21:30:02 +0800

    删除编译

--- MDK-ARM/Frame_T/app_tool.d
+++
@@ -1,5 +0,0 @@
-frame_t/app_tool.o: ..\RC10_LIB\APP\Src\APP_tool.cpp \
-  ..\RC10_LIB\APP\Inc\APP_tool.h \
-  ..\Middlewares\ST\ARM\DSP\Inc\arm_math.h \
-  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
-  ..\Drivers\CMSIS\Include\cmsis_armclang.h
