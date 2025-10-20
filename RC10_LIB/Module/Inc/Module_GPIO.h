ba992c7fb8a255ff24d2b73e490edd3a372b367c
70er66 <2602347865@qq.com>
2025-10-20 20:37:50 +0800

    GPIO类的简单封装

--- RC10_LIB/Module/Inc/Module_GPIO.h
+++ RC10_LIB/Module/Inc/Module_GPIO.h
@@ -1,3 +1,13 @@
+/**
+ * @file Moudle_GPIO.h
+ * @author 70er66
+ * @brief 达妙电机类
+ * @version 1.0
+ * 
+ * 此文件包含达妙J4310封装
+ */
+ 
+ 
 #ifndef __MODULE_GPIO_H
 #define __MODULE_GPIO_H
 #ifdef __cplusplus
@@ -13,11 +23,15 @@ extern "C" {
 
 #ifdef __cplusplus
 
-
-class GPIO {
+//-------------------------------------------------------------------------
+//*GPIO口的类封装；
+//*使用前需要在Cubemx上配置好对应的GPIO口
+//*在实例化创建对象的时候输入对应的GPIO口
+//*GPIO口的中断管理后续添加；
+class GPIODevice {
 public:
-	GPIO(GPIO_TypeDef* port,uint16_t pin);
-	~GPIO();
+	GPIODevice(GPIO_TypeDef* port,uint16_t pin);
+	~GPIODevice();
 void Set_pin();    
 void Reset_pin();
 void Toggle_pin();
